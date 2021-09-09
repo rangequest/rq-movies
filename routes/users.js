46e5320ec496ff5c6e16ed2b021eeedd38bd51b9
Range Quest <range.quest@gmail.com>
2021-09-09 08:22:18 +0530

    authorization handled using a middleware called auth.

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,12 @@ const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
