009a8dfa93e1954b7033843c9cfd4c909ee1e73f
Range Quest <range.quest@gmail.com>
2021-09-09 07:44:26 +0530

    added user endpoint.

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const movies = require("./routes/movies");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 const mongoose = require("mongoose");
+const config = require("config");
+
+if (!config.get("jwtPrivateKey")) {
+  console.log("FATAL ERROR: jwtPrivateKey is not defined");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/rq-movies")
@@ -19,6 +27,8 @@ app.use("/api/movies/", movies);
 app.use("/api/genres/", genres);
 app.use("/api/customers/", customers);
 app.use("/api/rentals/", rentals);
+app.use("/api/users/", users);
+app.use("/api/auth/", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const { User, validate } = require("../models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registred");
+
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+    const token = user.generateAuthToken();
+    res
+      .header("x-auth-token", token)
+      .send(_.pick(user, ["_id", "name", "email"]));
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+module.exports = router;
