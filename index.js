472c7485981fb4640a4b646c803630dc422a64ee
Range Quest <range.quest@gmail.com>
2021-09-11 14:24:25 +0530

    installed helmet and compression.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./startup/routes')(app)
 require('./startup/db')()
 require('./startup/config')()
 require('./startup/validation')()
+require('./startup/prod')(app)
 
 //For testing purposes...
 //throw new Error("Uncaught exception");
