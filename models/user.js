8e51b9927a03ba51265a6974a17b5dd1122babf8
Range Quest <range.quest@gmail.com>
2021-09-16 23:27:06 +0530

    Included more properties to the jwt.

--- models/user.js
+++ models/user.js
@@ -26,7 +26,10 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.methods.generateAuthToken = function () {
-  return jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'))
+  return jwt.sign(
+    { _id: this._id, name: this.name, email: this.email, isAdmin: this.isAdmin },
+    config.get('jwtPrivateKey')
+  )
 }
 
 const User = mongoose.model('User', userSchema)
