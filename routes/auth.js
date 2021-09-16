4eac0429873a270f57a79d2b112a7bb4538d8f10
Range Quest <range.quest@gmail.com>
2021-09-16 23:26:47 +0530

    Prevent going further after not valid password.

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
   if (!user) return res.status(400).send('Invalid email or password')
 
   const validPassword = await bcrypt.compare(req.body.password, user.password)
-  if (!validPassword) res.status(400).send('Invalid email or password')
+  if (!validPassword) return res.status(400).send('Invalid email or password')
 
   res.send(user.generateAuthToken())
 })
