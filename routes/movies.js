2a3df62cbdafa71b6dce610a82cf91c0afdac07b
Range Quest <range.quest@gmail.com>
2021-09-16 17:39:16 +0530

    Movies post authorization.

--- routes/movies.js
+++ routes/movies.js
@@ -3,13 +3,14 @@ const { Genre } = require('../models/genre')
 const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/auth')
 
 router.get('/', async (req, res) => {
   const movies = await Movie.find().sort('name')
   res.send(movies)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
