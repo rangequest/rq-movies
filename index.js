29b2aa77c61fa1dc8cb20dde81ef34c2243a0b0e
Range Quest <range.quest@gmail.com>
2021-09-16 17:38:45 +0530

    Added configurable port.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const config = require('config')
 const logger = require('./logger')
 
 require('./startup/logging')()
@@ -9,16 +10,9 @@ require('./startup/config')()
 require('./startup/validation')()
 require('./startup/prod')(app)
 
-//For testing purposes...
-//throw new Error("Uncaught exception");
-//const p = Promise.reject(new Error("Unhandled Promise Rejection"));
-
-// For testing logger
-// const error = new Error('Ooops')
-// logger.error('An error occurred:', error)
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || config.get('port')
 const server = app.listen(port, () =>
   logger.log({ level: 'debug', message: `Server is listening on port ${port}...` })
 )
+
 module.exports = server
