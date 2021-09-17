1325c5fe4c5c5507a182341644dc1b9661097376
Range Quest <range.quest@gmail.com>
2021-09-17 08:26:19 +0530

    Protected movie delete route.

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,9 @@
 const { Movie, validate } = require('../models/movie')
 const { Genre } = require('../models/genre')
-const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/auth')
+const admin = require('../middleware/admin')
 
 router.get('/', async (req, res) => {
   const movies = await Movie.find().sort('name')
@@ -31,7 +31,7 @@ router.post('/', auth, async (req, res) => {
   res.send(movie)
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
@@ -57,7 +57,7 @@ router.put('/:id', async (req, res) => {
   res.send(movie)
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, admin, async (req, res) => {
   const movie = await Movie.findByIdAndRemove(req.params.id)
 
   if (!movie) return res.status(404).send('The movie with the given ID was not found.')
